b501c7d9c288808fe0aa9308f173e1fa94a50e63
Michał <ciezczakmichal@users.noreply.github.com>
2022-04-12 19:37:29 +0200

    Pobieranie danych ze wskazanego adresu

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,54 @@
 <script lang="ts">
     import { onMount } from 'svelte'
+    import Content from './Content.svelte'
     import { getHashValue } from './get-hash-value'
 
-    let dataSourceUrl: string = ''
+    enum Status {
+        Loading,
+        DataDisplay,
+        Error,
+    }
 
-    function updateDataSource() {
-        dataSourceUrl = getHashValue('data-source')
+    let status = Status.Loading
+    let errorMessage = ''
+
+    let url: string = ''
+    let data: object = {}
+
+    async function fetchData(): Promise<object> {
+        let response
+
+        try {
+            response = await fetch(url)
+        } catch (error) {
+            const message = error instanceof Error ? error.message : ''
+            throw new Error(`Błąd połączenia sieciowego (${message})`)
+        }
+
+        try {
+            return await response.json()
+        } catch {
+            throw new Error('Dane nie są w formacie JSON')
+        }
+
+        // @todo walidacja zgodności z formatem
+    }
+
+    async function updateDataSource(): Promise<void> {
+        status = Status.Loading
+        errorMessage = ''
+        url = getHashValue('data-source')
+        data = {}
+
+        try {
+            data = await fetchData()
+            status = Status.DataDisplay
+        } catch (error) {
+            if (error instanceof Error) {
+                status = Status.Error
+                errorMessage = error.message
+            }
+        }
     }
 
     onMount(() => {
@@ -16,8 +59,12 @@
 
 <main>
     <h1>Solar Data Viewer</h1>
-    <div><b>Źródło danych:</b> {dataSourceUrl}</div>
-</main>
 
-<style>
-</style>
+    {#if status === Status.Loading}
+        <h3>Trwa pobieranie danych...</h3>
+    {:else if status === Status.Error}
+        <p>Pobranie danych nie powiodło się. {errorMessage}.</p>
+    {:else}
+        <Content {url} {data} />
+    {/if}
+</main>
---
+++ src/Content.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+
+    export let url: string
+    export let data: object
+
+    onMount(() => console.log(data))
+</script>
+
+<div>
+    <div><b>Źródło danych:</b> {url}</div>
+</div>
