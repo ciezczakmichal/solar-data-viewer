11546978d2ff363b98e8d67118a32261ab898d39
Michał <ciezczakmichal@users.noreply.github.com>
2022-04-14 19:43:44 +0200

    Wykres produkcji: widok tygodniowy lub miesięczny

--- ui/src/components/ProductionGraph.svelte
+++ ui/src/components/ProductionGraph.svelte
@@ -1,22 +1,63 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { Chart } from 'chart.js'
-    import { EnergyProducedInfoDateFormat, type DataFormat } from 'format'
+    import {
+        EnergyProducedInfo,
+        EnergyProducedInfoDateFormat,
+        type DataFormat,
+    } from 'format'
     import { parseDate } from 'calculation'
 
     export let data: DataFormat
 
-    const valuesToUse = data.energyProduced.filter(
-        item => parseDate(item.date, EnergyProducedInfoDateFormat).day() === 0 // niedziela
-    )
+    enum DataRange {
+        Week,
+        Month,
+    }
 
-    let chartValues = valuesToUse.map(item => item.value)
-    let chartLabels = valuesToUse.map(item => item.date)
-
+    let chart: Chart | null = null
     let chartCanvas: HTMLCanvasElement
+    let dataRange = DataRange.Week
+
+    function handleSwitchRange() {
+        dataRange =
+            dataRange === DataRange.Week ? DataRange.Month : DataRange.Week
+    }
+
+    $: switchButtonText =
+        'Przełącz na widok ' +
+        (dataRange === DataRange.Week ? 'miesięczny' : 'tygodniowy')
+
+    let valuesToUse: EnergyProducedInfo[] = []
+    let chartValues: number[] = []
+    let chartLabels: string[] = []
+
+    $: {
+        if (dataRange === DataRange.Week) {
+            valuesToUse = data.energyProduced.filter(
+                item =>
+                    parseDate(item.date, EnergyProducedInfoDateFormat).day() ===
+                    0 // niedziela
+            )
+        } else {
+            valuesToUse = data.energyProduced.filter(item => {
+                const date = parseDate(item.date, EnergyProducedInfoDateFormat)
+                return date.date() === date.daysInMonth()
+            })
+        }
+
+        chartValues = valuesToUse.map(item => item.value)
+        chartLabels = valuesToUse.map(item => item.date)
+
+        if (chart) {
+            chart.data.labels = chartLabels
+            chart.data.datasets[0].data = chartValues
+            chart.update()
+        }
+    }
 
     onMount(() => {
-        const chart = new Chart(chartCanvas, {
+        chart = new Chart(chartCanvas, {
             type: 'line',
             data: {
                 labels: chartLabels,
@@ -34,5 +75,21 @@
 </script>
 
 <div>
+    <div class="graph-header">
+        <h3>Wykres energii wyprodukowanej</h3>
+        <div>
+            <button on:click={handleSwitchRange}>{switchButtonText}</button>
+        </div>
+    </div>
     <canvas bind:this={chartCanvas} width="4" height="1" />
 </div>
+
+<style>
+    .graph-header {
+        display: flex;
+    }
+
+    .graph-header h3 {
+        flex-grow: 1;
+    }
+</style>
