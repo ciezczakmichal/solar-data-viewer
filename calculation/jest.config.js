a9cf70feb319bf9e54b5b1dc29bd9b135c58a0ff
Michał <ciezczakmichal@users.noreply.github.com>
2022-04-22 21:09:03 +0200

    Poprawka konfiguracji do obsługi ESM w Jest

--- calculation/jest.config.js
+++ calculation/jest.config.js
@@ -1,7 +1,13 @@
 export default {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/default-esm',
     rootDir: '.',
     testEnvironment: 'node',
     setupFilesAfterEnv: ['jest-extended'],
     testPathIgnorePatterns: ['dist'],
+
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
 }
