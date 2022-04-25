36e9762ebe5dea09336d13f9d13fde8db117d03a
Michał <ciezczakmichal@users.noreply.github.com>
2022-04-25 20:52:47 +0200

    UI: config Jest - dodanie linków

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,3 +1,6 @@
+// w przyszłości to się może przydać:
+// https://daveceddia.com/svelte-typescript-jest/
+// https://dev.to/robole/testing-svelte-components-with-jest-and-vite-219d
 export default {
     preset: 'ts-jest/presets/default-esm',
     rootDir: '.',
