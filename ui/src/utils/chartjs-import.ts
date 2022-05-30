2114cdbf483825673b5bd92189dcc891803de611
Michał <ciezczakmichal@users.noreply.github.com>
2022-05-30 15:35:21 +0200

    UI: importowanie tylko używanych składowych chart.js

--- ui/src/utils/chartjs-import.ts
+++ ui/src/utils/chartjs-import.ts
@@ -1,1 +1,31 @@
-import 'chart.js/auto'
+// importowanie wszystkich składowych chart.js
+// import 'chart.js/auto
+
+// importowanie wybranych składowych chart.js w celu zmniejszenia rozmiaru aplikacji
+import {
+    BarController,
+    BarElement,
+    CategoryScale,
+    Chart,
+    Legend,
+    LinearScale,
+    LineController,
+    LineElement,
+    PointElement,
+    Tooltip,
+} from 'chart.js'
+
+Chart.register(
+    BarElement,
+    LineElement,
+    PointElement,
+
+    BarController,
+    LineController,
+
+    LinearScale,
+    CategoryScale,
+
+    Legend,
+    Tooltip
+)
