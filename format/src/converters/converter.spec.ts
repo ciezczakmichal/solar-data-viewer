83b8359f17b625ef38436b90496da2c7fba6fe8a
Michał <ciezczakmichal@users.noreply.github.com>
2022-06-08 19:17:04 +0200

    Format: udane uruchomienie testów konwertera, dopisanie kolejnych

--- format/src/converters/converter.spec.ts
+++ format/src/converters/converter.spec.ts
@@ -1,16 +1,44 @@
 // wymagane przez class-transformer
 import 'reflect-metadata'
 
-// import { convertObjectToDataFormat } from './converter'
+import { convertObjectToDataFormat } from './converter'
 
 describe('convertObjectToDataFormat', () => {
-    it('test, który niczego nie testuje :(', () => {})
+    function testValueThrowsError(value: any): Promise<void> {
+        return expect(() =>
+            convertObjectToDataFormat(value)
+        ).rejects.toThrowError()
+    }
 
-    // @todo jest problem z uruchomieniem testów - błędy Jest
-    // "SyntaxError: The requested module X does not provide an export named Y"
-    // it('null i undefined nie jest poprawnym formatem danych', async () => {
-    // await expect(() =>
-    //     convertObjectToDataFormat(null)
-    // ).rejects.toThrowError()
-    // })
+    it('null nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError(null)
+    })
+
+    it('undefined nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError(undefined)
+    })
+
+    it('boolean nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError(false)
+        await testValueThrowsError(true)
+    })
+
+    it('liczba nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError(0)
+        await testValueThrowsError(1.23)
+    })
+
+    it('string nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError('')
+        await testValueThrowsError(' ')
+        await testValueThrowsError('test')
+    })
+
+    it('tablica nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError([])
+    })
+
+    it('pusty obiekt nie jest poprawnym formatem danych', async () => {
+        await testValueThrowsError({})
+    })
 })
--- format/src/converters/generics.ts
+++ format/src/converters/generics.ts
@@ -11,8 +11,7 @@ export async function convertPlainObjectToInstance<T>(
     errorCreator: (error: any) => Error,
     options?: ClassTransformOptions
 ): Promise<T> {
-    // plainToInstance() nie rzuci wyjątku, jeśli dane są tablicą
-    // dodatkowe testy czy zmienna jest obiektem nie zaszkodzą
+    // plainToInstance() nie rzuci wyjątku, jeśli dane są tablicą, boolean lub liczbą
     if (typeof plain !== 'object' || Array.isArray(plain) || plain === null) {
         throw errorCreator('Główny element nie jest obiektem JSON')
     }
