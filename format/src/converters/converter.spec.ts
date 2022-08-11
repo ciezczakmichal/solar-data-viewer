f13c9169f55b783b1a4621e786078776ed26d899
Michał <ciezczakmichal@users.noreply.github.com>
2022-08-11 11:25:12 +0200

    Format: dodanie testu weryfikującego źródło z minimalną liczbą danych

--- format/src/converters/converter.spec.ts
+++ format/src/converters/converter.spec.ts
@@ -1,44 +1,59 @@
 // wymagane przez class-transformer
 import 'reflect-metadata'
 
+import { readFile } from 'fs/promises'
+import { DataFormat } from '../definitions/data-format'
 import { convertObjectToDataFormat } from './converter'
 
 describe('convertObjectToDataFormat', () => {
-    function testValueThrowsError(value: any): Promise<void> {
-        return expect(() =>
-            convertObjectToDataFormat(value)
-        ).rejects.toThrowError()
-    }
+    describe('testy błędnych, prostych wartości', () => {
+        function testValueThrowsError(value: any): Promise<void> {
+            return expect(() =>
+                convertObjectToDataFormat(value)
+            ).rejects.toThrowError()
+        }
 
-    it('null nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError(null)
+        it('null nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError(null)
+        })
+
+        it('undefined nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError(undefined)
+        })
+
+        it('boolean nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError(false)
+            await testValueThrowsError(true)
+        })
+
+        it('liczba nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError(0)
+            await testValueThrowsError(1.23)
+        })
+
+        it('string nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError('')
+            await testValueThrowsError(' ')
+            await testValueThrowsError('test')
+        })
+
+        it('tablica nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError([])
+        })
+
+        it('pusty obiekt nie jest poprawnym formatem danych', async () => {
+            await testValueThrowsError({})
+        })
     })
 
-    it('undefined nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError(undefined)
-    })
+    it('przykładowy plik z minimalną liczbą danych jest traktowany jako poprawny', async () => {
+        const json = await readFile(
+            new URL('./tests/example-smallest-data.json', import.meta.url),
+            'utf8'
+        )
+        const data = JSON.parse(json)
 
-    it('boolean nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError(false)
-        await testValueThrowsError(true)
-    })
-
-    it('liczba nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError(0)
-        await testValueThrowsError(1.23)
-    })
-
-    it('string nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError('')
-        await testValueThrowsError(' ')
-        await testValueThrowsError('test')
-    })
-
-    it('tablica nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError([])
-    })
-
-    it('pusty obiekt nie jest poprawnym formatem danych', async () => {
-        await testValueThrowsError({})
+        const result = await convertObjectToDataFormat(data)
+        expect(result).toBeInstanceOf(DataFormat)
     })
 })
