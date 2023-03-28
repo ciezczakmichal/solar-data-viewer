9b0ad8d39038805b574592a059095ada01210836
ciezczakmichal <ciezczakmichal@users.noreply.github.com>
2023-03-28 16:59:50 +0200

    UI: aktualizacja komentarza

--- ui/src/routes/+layout.ts
+++ ui/src/routes/+layout.ts
@@ -1,5 +1,5 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true
 
-// po usunięciu odwołań do document można się tego pozbyć
+// zależności CommonJS (takie jak runtime ajv oraz ajv-i18n) blokują usunięcie tej opcji
 export const ssr = false
