059ca419b1010de6a4747dbff3d531678cbdcbe6
ciezczakmichal <ciezczakmichal@users.noreply.github.com>
2023-12-05 13:36:40 +0100

    UI: przywrócenie działania testu

--- ui/tests/demo-data.spec.ts
+++ ui/tests/demo-data.spec.ts
@@ -1,11 +1,14 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import { readFile } from 'fs/promises'
 import { validateSolarData } from 'schema'
 
+const filename = fileURLToPath(import.meta.url)
+const dir = dirname(filename)
+
 async function getData(): Promise<Record<string, any>> {
-    const json = await readFile(
-        new URL('../static/demo-data.json', import.meta.url),
-        'utf8'
-    )
+    const path = join(dir, '../static/demo-data.json')
+    const json = await readFile(path, 'utf8')
     return JSON.parse(json)
 }
 
