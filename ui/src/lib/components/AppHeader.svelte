4072b24dda62cde8ef75a91466aac2f34a4700ec
ciezczakmichal <ciezczakmichal@users.noreply.github.com>
2023-12-14 15:31:01 +0100

    Przycisk zmiany motywu

--- ui/src/lib/components/AppHeader.svelte
+++ ui/src/lib/components/AppHeader.svelte
@@ -1,10 +1,12 @@
 <script>
     import AppLogo from './AppLogo.svelte'
+    import ThemeChangeButton from './ThemeChangeButton.svelte'
 </script>
 
 <header class="mb-4 mt-2 flex items-center">
     <div class="ml-2 mr-4">
         <AppLogo />
     </div>
-    <h1 class="text-3xl font-bold">Solar Data Viewer</h1>
+    <h1 class="flex-1 text-3xl font-bold">Solar Data Viewer</h1>
+    <ThemeChangeButton />
 </header>
