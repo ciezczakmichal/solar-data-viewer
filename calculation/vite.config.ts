b23cf8ca907a20a6b5e815b9c08132ac7edc3966
ciezczakmichal <ciezczakmichal@users.noreply.github.com>
2025-10-24 15:02:59 +0200

    fix(calculation): aktualizacja ścieżki po zmianie katalogu głównego

--- calculation/vite.config.ts
+++ calculation/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        setupFiles: ['./src/utils/dayjs-import.ts'],
+        setupFiles: ['./lib/utils/dayjs-import.ts'],
     },
 })
